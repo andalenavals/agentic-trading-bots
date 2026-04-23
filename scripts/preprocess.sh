a94282a3d18e5da0c047ed610c5fbea5b91796a5
andalenavals <a.navarro.alsina@gmail.com>
2026-04-23 13:50:09 +0200

    Use deploy preprocessing config for Pages

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -eu
 
+CONFIG_PATH="${PREPROCESS_CONFIG:-configs/preprocessing/default.json}"
+
 if [ -x ".venv/bin/python" ]; then
   PYTHON_BIN=".venv/bin/python"
 else
   PYTHON_BIN="python3"
 fi
 
-exec "$PYTHON_BIN" -m agentic_trading.preprocessing --config configs/preprocessing/default.json "${1:-all}"
+exec "$PYTHON_BIN" -m agentic_trading.preprocessing --config "$CONFIG_PATH" "${1:-all}"
